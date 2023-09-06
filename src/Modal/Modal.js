6f9402a09fe7ac57752b3e245494956cdd264607
Nataliia <Nata13pr@gmail.com>
2023-09-06 13:10:46 +0300

    Needed to add remobeEventListener-fixed previous bag

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -8,20 +8,22 @@ export default class Modal extends Component {
   componentDidMount() {
     console.log(" Modal componentDidMount");
 
-    window.addEventListener("keydown", (e) =>{
-      console.log(e.code);
-      if (e.code === 'Escape') {
-        console.log("Pressed Escape");
-
-        this.props.onClose();
-      }
-    });
+    window.addEventListener("keydown",this.handleKeyDown);
   }
 
   componentWillUnmount() {
     console.log("componentWillUnmount");
+
+    window.removeEventListener('keydown',this.handleKeyDown)
   }
 
+  handleKeyDown =e=>{
+    if (e.code === 'Escape') {
+      console.log("Pressed Escape");
+
+      this.props.onClose();
+    }
+  }
   render() {
     return createPortal(
       <div className="Modal__backdrop">
